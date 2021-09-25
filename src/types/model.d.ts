7cc33e1d0d0daafbb526587ce52fb87b99701850
Ariel Ril <arielril@gmail.com>
2021-09-25 20:25:52 -0300

    added initial api documentation

--- src/types/model.d.ts
+++ src/types/model.d.ts
@@ -1,6 +1,10 @@
 export type Node = {
   uid: string; // unique identifier
   name?: string; // name for better identification
+  edges: {
+    goFrom: Edge[];
+    comeTo: Edge[];
+  }[];
   service?: string; // running service (can be used in the place of `name`)
   port?: number; // port of the service that is running
   tags?: string[];
@@ -14,6 +18,8 @@ export type Node = {
 export type Edge = {
   uid: string; // unique identifier
   label?: string; // human readable edge name
+  originNode: Node;
+  destinationNode: Node;
   weight: number; // default: 0
   tags?: string[];
 };
